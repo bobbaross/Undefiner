4778a550b9f00e4de225d78a7284cbffcb9cee6a
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-22 18:39:08 +0200

    Update expiredb.js

--- functions/expiredb.js
+++ functions/expiredb.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 // don't question it
-const Schema = new mongoose.Schema({
+const ExpireSchema = new mongoose.Schema({
     type: String,
     db: String,
     entries: Array
 });
 
-const entryType = mongoose.model("type", Schema);
-const entryDB = mongoose.model("db", Schema);
+const entryType = mongoose.model("type", ExpireSchema);
+const entryDB = mongoose.model("db", ExpireSchema);
 
 module.exports = { name: "db", entryType, entryDB }
