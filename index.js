3b3b0b95e7506375c45afa24bcf2e5af2a4806b0
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-22 19:19:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const gad = require('git-auto-deploy');
 
-app.listen(9003, () => console.log(`Webhook Running!`));
+app.listen(9004, () => console.log(`Webhook Running!`));
 
 app.post('/aprixia/undefiner/webhook', (req,res) => {
     if (req.headers.secret === webhookSecret) {
