a8177ad9f9187751c1aff2785762633df66ace4b
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-22 19:57:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const {webhookSecret} = require('./config.json');
-const http = require('http');
+const app = require('express')();
 const crypto = require('crypto');
 const exec = require('child_process').exec;
-
-http.createServer(function (req, res) {
-    req.on('data', async function (chunk) {
-        let sig = "sha1=" + crypto.createHmac('sha1', webhookSecret).update(chunk.toString()).digest('hex');
-
-        if (req.headers['x-hub-signature'] == sig) {
-            await exec('git pull');
-            process.exit();
-        }
-    });
-
-    res.end();
-}).listen(9004);
-
+const port = 9094;
+app.post("*", (req, res) => {
+    req.on("data", async function (chunk) {
+            await handle(res, req.headers["x-hub-signature"], webhookSecret, chunk)
+        return;
+    })
+}).listen(port);
+app.get("*", () => {
+    return;
+})
 const shard = require('./shard.js');
-
+async function handle(res, header, conf, chunk) {
+    let sig = "sha1=" + crypto.createHmac('sha1', conf).update(chunk.toString()).digest('hex');
+    if (header === sig) {
+        await exec(`git fetch --all && git reset --hard origin/master && pm2 restart Undefiner`,
+            (err, out) => {
+                console.log(out)
+            });
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(403)
+    }
+}
 try {
     shard();
 } catch (error) {
