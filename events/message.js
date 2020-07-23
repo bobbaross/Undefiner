88801bf96b79bb670a7f0e36887b727e352271be
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-23 14:04:16 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,9 +1,11 @@
+const { Utils } = require(`../functions/functions.js`);
+
 module.exports = (client, message) => {
     var { commandHandler } = require('../functions/commandhandler.js');
     if (message.channel.type === 'dm') {
         commandHandler(client, message, "undefine ").catch(err => console.error(err));
     } else {
-        var { getDB } = require('../functions/functions.js');
+        var { getDB } = new Utils(client);
         getDB(message.guild.id).then(res => {
             if (res && res.prefix) var prefix = res.prefix;
             else var prefix = "undefine ";
