0bdb1b05a04986e8c436d3786cc6ed0cb2ad8512
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-23 14:37:01 +0200

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -1,5 +1,6 @@
 const {MessageEmbed} = require('discord.js');
 const { Utils } = require('../functions/functions');
+const {branding} = require('../config.json').colors;
 
 module.exports = {
     name: "settings",
@@ -33,7 +34,25 @@ module.exports = {
             return errbed();
         }
         getDB(message.guild.id).then(async res => {
-            return;
+            if (!res) {
+                res = await createDB(message.guild.id);
+                await saveDB(res);
+            }
+            var embed;
+            var value;
+            switch (args[0]) {
+                default:
+                    embed = new MessageEmbed()
+                    .setColor(branding)
+                    .setTitle(`Settings`)
+                    .addField(`Prefix`, `${res.prefix ? res.prefix : `Hmm... Something doesn't seem right here... Please report this to the developers at the [AprixStudios Discord](https://discord.gg/RpM43Gc)`}\nThis value must be at least 1 letter and at max 10 and may not include spaces. Set the value to \`-reset\` to reset it back to the original prefix.`)
+                    .addField(`Muted Role`, `${getRole(res.settings.mutedRole, message.guild.roles) ? getRole(res.settings.mutedRole, message.guild.roles).name : `Not set.`}\nThis value is changable anytime, but will be set automatically upon a mute.`)
+                    .addField(`Modlogs`, `${getChannel(res.settings.modLogs, message.guild.channels) ? getChannel(res.settings.modLogs, message.guild.channels).name : `None`}\nSetting this value to a channel will enable mod logs to be sent in that channel.\nSetting this value to \`this\` will make it the current channel\nSetting this value to \`there\` will set it to be in the channel where the command was sent.\nSetting this value to anything not specified in this embed will turn mod logs off.`)
+
+                    message.channel.send(branding).catch(err => err);
+                    break;
+            }
+
         });
     }
 }
