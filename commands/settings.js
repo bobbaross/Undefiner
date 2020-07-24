ad7807f35cdb0b1a9d93540844b75990282d8515
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-25 00:31:32 +0200

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -48,8 +48,8 @@ module.exports = {
                     .setColor(branding)
                     .setTitle(`Settings`)
                     .addField(`Prefix`, `${res.prefix ? res.prefix : `Hmm... Something doesn't seem right here... Please report this to the developers at the [AprixStudios Discord](https://discord.gg/RpM43Gc)`}\nThis value must be at least 1 letter and at max 10. Spaces can be used. Use \`_\` to add a guaranteed space.`)
-                    .addField(`Muted Role`, `${listingRole !== undefined ? listingRole.name : `Not set.`}\nThis value is changable anytime, but will be set automatically upon a mute.`)
-                    .addField(`Modlogs`, `${listingChan !== undefined ? listingChan.name : res.settings.modLogs}\nSetting this value to a channel will enable mod logs to be sent in that channel.\nSetting this value to \`this\` will make it the current channel\nSetting this value to \`there\` will set it to be in the channel where the command was sent.\nSetting this value to anything not specified in this embed will turn mod logs off.`)
+                    .addField(`Muted Role`, `${listingRole ? listingRole : `Not set.`}\nThis value is changable anytime, but will be set automatically upon a mute.`)
+                    .addField(`Modlogs`, `${listingChan ? listingChan : res.settings.modLogs}\nSetting this value to a channel will enable mod logs to be sent in that channel.\nSetting this value to \`this\` will make it the current channel\nSetting this value to \`there\` will set it to be in the channel where the command was sent.\nSetting this value to anything not specified in this embed will turn mod logs off.`)
 
                     message.channel.send(embed).catch(err => console.error(err));
                     break;
