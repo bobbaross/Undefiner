af8c8d6ff9de550eb4e7be92da000de23cf5fab6
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-25 00:29:34 +0200

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -44,12 +44,14 @@ module.exports = {
                 default:
                     let listingRole = getRole(res.settings.mutedRole, message.guild.roles);
                     let listingChan = getChannel(res.settings.modLogs, message.guild.channels);
+                    if (await listingRole) await listingRole;
+                    if (await listingChan) await listingChan;
                     embed = new MessageEmbed()
                     .setColor(branding)
                     .setTitle(`Settings`)
                     .addField(`Prefix`, `${res.prefix ? res.prefix : `Hmm... Something doesn't seem right here... Please report this to the developers at the [AprixStudios Discord](https://discord.gg/RpM43Gc)`}\nThis value must be at least 1 letter and at max 10. Spaces can be used. Use \`_\` to add a guaranteed space.`)
-                    .addField(`Muted Role`, `${listingRole ? listingRole.name : `Not set.`}\nThis value is changable anytime, but will be set automatically upon a mute.`)
-                    .addField(`Modlogs`, `${listingChan ? listingChan.name : res.settings.modLogs}\nSetting this value to a channel will enable mod logs to be sent in that channel.\nSetting this value to \`this\` will make it the current channel\nSetting this value to \`there\` will set it to be in the channel where the command was sent.\nSetting this value to anything not specified in this embed will turn mod logs off.`)
+                    .addField(`Muted Role`, `${listingRole !== undefined ? listingRole.name : `Not set.`}\nThis value is changable anytime, but will be set automatically upon a mute.`)
+                    .addField(`Modlogs`, `${listingChan !== undefined ? listingChan.name : res.settings.modLogs}\nSetting this value to a channel will enable mod logs to be sent in that channel.\nSetting this value to \`this\` will make it the current channel\nSetting this value to \`there\` will set it to be in the channel where the command was sent.\nSetting this value to anything not specified in this embed will turn mod logs off.`)
 
                     message.channel.send(embed).catch(err => console.error(err));
                     break;
