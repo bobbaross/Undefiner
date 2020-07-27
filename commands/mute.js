78f7fe41905a2c577d072745c44d232f57acbcdf
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-28 00:23:07 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -127,7 +127,7 @@ module.exports = {
                 console.log('R');
                 utils.getEntries("mute").then(async activeMutes => {
                     console.log('T');
-                    activeMutes.entires.push({
+                    activeMutes.entries.push({
                         guildId: message.guild.id,
                         userId: user.id,
                         time: time,
