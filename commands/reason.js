41bfd732b0ca15400745756d91408b095339a30f
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-29 11:05:57 +0200

    Update reason.js

--- commands/reason.js
+++ commands/reason.js
@@ -50,8 +50,7 @@ module.exports = {
             console.log(index)
             res.modCases[index].reason = reason;
             console.log(res.modCases[index])
-            await res;
-            utils.saveDB(res).catch(err => console.error(err));
+            await utils.saveDB(res).catch(err => console.error(err));
 
             message.channel.messages.fetch(`${modCase.embedId}`).then(msg => {
                 if (!msg) {
