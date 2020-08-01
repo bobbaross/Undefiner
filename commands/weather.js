3b1c4cf3f8ee856ceaf790cb81a171e52789d3fd
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-02 00:42:02 +0200

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -1,6 +1,6 @@
 const {MessageEmbed} = require('discord.js');
 const weather = require('weather-js');
-const { Utils } = require('../functions/functions');
+const {branding} = require('../config.json').colors;
 
 module.exports = {
     name: "weather",
@@ -8,8 +8,52 @@ module.exports = {
     usage: "<C/F> <city name>",
     aliases: [],
     category: "miscellaneous",
-    
+
     async undefine(client, message, args) {
-        utils = new Utils(client);
+        var degreeType = args[0];
+        if (!degreeType) {
+            let embed = new MessageEmbed()
+            .setDescription(`Hey, I need to know whether you'd like Celsius or Fahrenheit!\n${this.name} ${this.usage}`)
+            return message.channel.send(embed).catch(err => message.channel.send(embed.description));
+        }
+        await args.shift();
+        var city = args.join(' ');
+        if (!city) {
+            let embed = new MessageEmbed()
+            .setDescription(`Excuse me, which city again? **I promise I am not a creep...**!\n${this.name} ${this.usage}`)
+            return message.channel.send(embed).catch(err => message.channel.send(embed.description));
+        }
+        weather.find({ search: city, degreeType, degreeType}, (err,result) => {
+            if (err) return;
+            if (!result) {
+                let embed = new MessageEmbed()
+                .setDescription(`REEEEEEEEE I CAN'T FIND ANY RESULTS!!!`)
+                return message.channel.send(embed).catch(err => message.channel.send(embed.description));
+            }
+            var current = result[0].current;
+            var location = result[0].location;
+
+            const embed = new MessageEmbed()
+            .setDescription(`**${current.skytext}**`)
+            .setAuthor(`Weather for ${current.observationpoint}`)
+            .setThumbnail(current.imageUrl)
+            .setColor(branding)
+            .addField('Timezone', `UTC${location.timezone}`, true)
+            .addField('Degree type', location.degreetype, true)
+            .addField('Temperature', `${current.temperature} degrees`, true)
+            .addField('Feels like', `${current.feelslike} degrees`, true)
+            .addField('Winds', current.winddisplay, true)
+            .addField('Humidity', `${current.humidity}%`, true)
+
+            return message.channel.send(embed).catch(err => {
+                let fields = [];
+                for (i=0;i<embed.fields.length;i++) {
+                    fields.push(`**${embed.fields[i].name}**: ${embed.fields[i].value}`);
+                }
+                message.channel.send(`${embed.description}
+${fields.join('\n\n')}
+                `).catch(err => err);
+            });
+        });
     }
 }
