4683988994b6b5042bcad8a601af6e4dac0ab2c3
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-01 20:47:21 +0200

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -1,2 +1,16 @@
 const {MessageEmbed} = require('discord.js');
 const weather = require('weather-js');
+const { Utils } = require('../functions/functions');
+
+module.exports = {
+    name: "weather",
+    description: "Get weather information in a certain area.",
+    usage: "<C/F> <city name>",
+    aliases: [],
+    category: "miscellaneous",
+
+
+    async undefine(client, message, args) {
+        utils = new Utils(client);
+    }
+}
