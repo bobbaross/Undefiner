bd162d6019db7708f605d99e6dd8baee981d1ab6
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-02 09:44:46 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -77,6 +77,8 @@ module.exports = {
                     .setDescription(`The prefix may not be longer than 10 characters.\n${this.name} ${this.usage}`);
                     return message.channel.send(embed).catch(err => message.channel.send(embed.description).catch(err => err));
                 }
+                console.log(message.content)
+                console.log(message.content.search(/ $/))
                 if (message.content.search(/ $/)) newPrefix = newPrefix+' ';
                 let oldPrefix = res.prefix;
                 res.prefix = newPrefix;
