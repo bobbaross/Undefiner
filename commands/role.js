b46b451079e56a17fb978a13e8b0b04447b6f6b0
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-05 14:12:31 +0200

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,5 +1,6 @@
 const {MessageEmbed} = require('discord.js');
 const {Checks} = require('../functions/checks');
+const { Utils } = require('../functions/functions');
 
 module.exports = {
     name: "role",
@@ -10,10 +11,31 @@ module.exports = {
     guildOnly: true,
 
     async undefine(client, message, args) {
-        if(Checks.hasPermission(message.guild.me, "MANAGE_ROLES")) {
-            return message.channel.send('yes').catch(err => err);
-        } else {
-            return message.channel.send('no').catch(err => err);
+        if (!Checks.hasPermission(message.guild.me, "MANAGE_ROLES")) {
+            let embed = new MessageEmbed()
+                .setDescription(`Hey, perhaps sort out my permissions first? I need the Manage Roles permission.`);
+                return message.channel.send(embed).catch(err => message.channel.send(embed.description).catch(err => err));
         }
+        utils = new Utils(client);
+        utils.getDB(message.guild.id).then(res => {
+            if (!res) res = await utils.createDB(message.guild.id);
+            let bypassRoles = [];
+            for (let role of res.modRoles) {
+                bypassRoles.push(role);
+            }
+            for (let role of res.adminRoles) {
+                bypassRoles.push(role);
+            }
+            if (!Checks.hasPermission(message.guild.member, "MANAGE_ROLES") && !Checks.hasRole(message.guild.member, bypassRoles)) {
+                let embed = new MessageEmbed()
+                .setDescription(`I may be blind, but I don't see ${message.member.hasPermission("MANAGE_MESSAGES") ? "Whoops" : "Manage Messages"} amongst your permissions.`);
+                return message.channel.send(embed).catch(err => message.channel.send(embed.description).catch(err => err));
+            }
+            var user = await getUser(args[0]);
+            if (!user) {
+                if (args[0].search(/all/i)) user = "all";
+                return message.channel.send(`${user}`)
+            }
+        });
     }
 }
