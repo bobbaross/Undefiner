fc25aa079a524882c43e4beeea9748aa948c77dd
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-08 18:37:42 +0200

    Update forcetypable.js

--- commands/forcetypable.js
+++ commands/forcetypable.js
@@ -29,6 +29,7 @@ module.exports = {
                 members.forEach(member => {
                     let newNick = sanitizer(member.displayName);
                     let newMember = message.guild.members.cache.get(member.user.id);
+                    console.log('a')
                     newMember.setNickname(newNick).then(console.log).catch(err => failedMembers.push(member.user.tag));
                 });
                 await failedMembers;
