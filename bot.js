231e6620dbf216ca08f37c2b0cd548202aabd4e0
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-09 12:35:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-const {token,dburi,webhookSecret} = require('./config.json');
+const {token,dbuser,dbpass,webhookSecret} = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs-extra');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/undefiner", {useNewUrlParser: true, useUnifiedTopology: true}).then(console.log(`Database Connected...`)).catch(error => console.error(error));
+mongoose.connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/undefiner`, {useNewUrlParser: true, useUnifiedTopology: true}).then(console.log(`Database Connected...`)).catch(error => console.error(error));
 
 
 try {
