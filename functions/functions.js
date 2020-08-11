dcc885c418a1df413b9b14c98fb20b702e656ef2
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-11 14:18:49 +0200

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -311,7 +311,7 @@ class Utils {
             let staffRole = Object.entries(staffRoles)[i][1].role;
             roles.push(staffRole);
         }
-        if (memberRoles?.cache.some(r => roles.includes(r.id))) {
+        if (memberRoles?.some(r => roles.includes(r.id))) {
             return true;
         }
     }
