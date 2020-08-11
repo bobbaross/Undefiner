4e9503755acfd2c0d2c3745467d9cd4f1811770c
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-11 10:56:44 +0200

    Update clientvars.js

--- clientvars.js
+++ clientvars.js
@@ -11,6 +11,7 @@ module.exports = (client) => {
     client.version = version;
     client.commands = new Discord.Collection();
     client.languages = new Discord.Collection();
+    client.constantCommands = ["botinfo", "eval", "exec", "help", "prefix", "report", "settings", "setup", "suggest", "togglecommand"]
 
     var commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
     for (let file of commandFiles) {
@@ -30,4 +31,30 @@ module.exports = (client) => {
     } catch (error) {
         console.error(error);
     }
+
+    // Event handler
+    try {
+        let eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+        for (let file of eventFiles) {
+            try {
+                let event = require(`./events/${file}`);
+                client.on(file.slice(0, -3), event.bind(null, client));
+            } catch (error) {
+                return console.error(error);
+            }
+        }
+    } catch (error) {
+        return console.error(error);
+    }
+
+    let presence = require('./functions/presence.js');
+    process.on('message', message => {
+        if (message === "All Shards Ready") {
+            try {
+                presence(client);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    });
 }
