f91915ab7d641e749ac7668ad1cb739f2458a4d3
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-11 10:30:07 +0200

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,7 +1,7 @@
 module.exports = () => {
     const { token } = require(`./config.json`);
     const { ShardingManager } = require('discord.js');
-    const manager = new ShardingManager('./bot.js', { totalShards: 'auto', token: token });
+    const manager = new ShardingManager('./bot.js', { totalShards: '6', token: token });
 
     manager.spawn();
     manager.on('shardCreate', shard => {
