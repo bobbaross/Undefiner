47f8e53a0d4e18e5a061e7300ae5b88c340d49a1
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-11 14:27:28 +0200

    Update commandhandler.js

--- functions/commandhandler.js
+++ functions/commandhandler.js
@@ -1,3 +1,5 @@
+const { auth } = require("../commands/help");
+
 async function commandHandler(client, message, prefix, disabledCommands) {
     var mentionedBotPrefix = message.content.startsWith(`<@!${client.user.id}> `);
     if (mentionedBotPrefix) prefix = `<@!${client.user.id}> `;
@@ -7,7 +9,8 @@ async function commandHandler(client, message, prefix, disabledCommands) {
     var command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
     if (!command) return;
-    if (command.staffOnly === true && !await client.functions.authorized(command, message.author)) return;
+    let authorized = command.staffOnly === true ? await client.functions.authorized(command, message.author) : null 
+    if (command.staffOnly === true && !authorized) return;
     if (command.guildOnly === true && message.channel.type === 'dm') return;
     if (disabledCommands && disabledCommands.includes(commandName)) return;
     try {
