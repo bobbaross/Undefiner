326ba7a4438a2ad3fc71c15773e09090e2e6c165
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-03 09:06:47 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,7 +6,7 @@ module.exports = (client, guild) => {
             if (!res) res = await client.functions.createStaffDB();
             let sysChan = guild.systemChannel;
             if (res.bannedServers.find(item => item === guild.id)) {
-                let item = res.bannedServers.find(item => item === guild.id);
+                let item = res.infractions.find(item => item.guild === guild.id);
                 try {
                     if (sysChan.permissionOverwrites.get(client.user.id).allow.has("SEND_MESSAGES")) {
                         sysChan.send(`Hey! This server is banned from using this bot.\n**Reason**: ${item.reason}\n\nThink this is a mistake? Join https://discord.gg/k2PEWMw and go to the support channel and we help you out!`);
@@ -15,7 +15,7 @@ module.exports = (client, guild) => {
                 finally {guild.leave()}
             }
             else if (res.bannedOwners.find(item => item === guild.ownerID)) {
-                let item = res.bannedOwners.find(item => item === guild.ownerID);
+                let item = res.infractions.find(item => item.owner === guild.ownerID);
                 try {
                     if (sysChan.permissionOverwrites.get(client.user.id).allow.has("SEND_MESSAGES")) {
                         sysChan.send(`Hey! The owner of this server is banned from using this bot.\n**Reason**: ${item.reason}\n\nThink this is a mistake? Join https://discord.gg/k2PEWMw and go to the support channel and we help you out!`);
