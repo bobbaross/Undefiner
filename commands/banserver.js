51510024d8a83ba8f3a43ff7e5d40e5c0ce3ebea
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-03 09:02:10 +0200

    Update banserver.js

--- commands/banserver.js
+++ commands/banserver.js
@@ -75,7 +75,7 @@ module.exports = {
             .addField(`Owner`, `${theServer.owner?.tag ?? "Owner not cached"}`, true)
             .addField(`Moderator`, `${message.author.tag}`, true)
             .addField(`Reason`, `${reason}`)
-            client.functions.sendMessageToSupportServerChannel("648040594651742239", embed).catch(err => err);
+            client.functions.sendMessageToSupportServerChannel("724615821669957673", embed).catch(err => err);
             client.functions.leaveServer(theServer.id).catch(err => err);
         });
     }
