c670ce4d07e40ad3cb23fe0cc890b610f415b157
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-03 10:25:13 +0200

    dfghdf

--- functions/functions.js
+++ functions/functions.js
@@ -303,8 +303,12 @@ class Utils {
         });
     }
 
-    async sendMessageToSupportServerChannel(channelid, msg) {
-        this.client.shard.broadcastEval('if (this.guilds.cache.get("724602779053719693")) this.guilds.cache.get("724602779053719693").channels.cache.get("'+channelid+'").send("'+msg+'")').catch(err => console.error(err));
+    async sendMessageToSupportServerChannel(channelid, msg, isEmbed) {
+        if (isEmbed !== true) {    
+            this.client.shard.broadcastEval('if (this.guilds.cache.get("724602779053719693")) this.guilds.cache.get("724602779053719693").channels.cache.get("'+channelid+'").send("'+msg+'")').catch(err => console.error(err));
+        } else if (isEmbed === true) {
+            this.client.shard.broadcastEval('if (this.guilds.cache.get("724602779053719693")) this.guilds.cache.get("724602779053719693").channels.cache.get("'+channelid+'").send('+msg+')').catch(err => console.error(err));
+        }
     }
 
     async authorized(command, sender) {
