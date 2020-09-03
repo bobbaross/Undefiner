a798614064234279497d16e4acb5a586ac9ee687
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-03 11:23:12 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,17 @@
 module.exports = (client) => {
     console.log(`Running!`);
     async function isBanned() {
-        client.functions.getStaffDB().then(async res => {
-            if (!res) res = await client.functions.createStaffDB();
-            for (let id of res.bannedServers) {
-                if (client.guilds.cache.get(id)) client.guilds.cache.get(id).leave();
-            }
-            for (let id of res.bannedOwners) {
-                if (client.guilds.cache.find(guild => guild.ownerID === id)) client.guilds.cache.find(guild => guild.ownerID = id).leave();
-            }
-        });
+        setTimeout(() => {
+            client.functions.getStaffDB().then(async res => {
+                if (!res) res = await client.functions.createStaffDB();
+                for (let id of res.bannedServers) {
+                    if (client.guilds.cache.get(id)) client.guilds.cache.get(id).leave();
+                }
+                for (let id of res.bannedOwners) {
+                    if (client.guilds.cache.find(guild => guild.ownerID === id)) client.guilds.cache.find(guild => guild.ownerID = id).leave();
+                }
+            });
+        }, 5000);
     }
     isBanned();
 }
