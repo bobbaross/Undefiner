6177c53ec6a685ec0ac86e07ccb6608f4a3c3530
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-03 09:26:30 +0200

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -4,14 +4,14 @@ module.exports = (client, oldMessage, newMessage) => {
     async function commands() {
         var { commandHandler } = require('../functions/commandhandler.js');
         if (newMessage.channel.type === 'dm') {
-            commandHandler(client, newMessage, "undefine ", null).catch(err => console.error(err));
+            commandHandler(client, newMessage, "undefine ", null, true).catch(err => console.error(err));
         } else {
             client.functions.getDB(newMessage.guild.id).then(res => {
                 if (res && res.prefix) var prefix = res.prefix;
                 else var prefix = "undefine ";
                 if (res && res.disabledCommands) var disabledCommands = res.disabledCommands;
                 else var disabledCommands = null;
-                commandHandler(client, newMessage, prefix, disabledCommands).catch(err => console.error(err));
+                commandHandler(client, newMessage, prefix, disabledCommands, false).catch(err => console.error(err));
             });
         }
     }
