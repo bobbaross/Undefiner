ab205c1274cc2a8d0b898c2b68dad92ec76ef254
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-03 09:04:49 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,10 +2,11 @@ const message = require("./message");
 
 module.exports = (client, guild) => {
     async function isBanned() {
-        client.functions.getStaffDB().then(res => {
+        client.functions.getStaffDB().then(async res => {
+            if (!res) res = await client.functions.createStaffDB();
             let sysChan = guild.systemChannel;
-            if (res?.bannedServers.find(item => item.guild === guild.id)) {
-                let item = res.bannedServers.find(item => item.guild === guild.id);
+            if (res.bannedServers.find(item => item === guild.id)) {
+                let item = res.bannedServers.find(item => item === guild.id);
                 try {
                     if (sysChan.permissionOverwrites.get(client.user.id).allow.has("SEND_MESSAGES")) {
                         sysChan.send(`Hey! This server is banned from using this bot.\n**Reason**: ${item.reason}\n\nThink this is a mistake? Join https://discord.gg/k2PEWMw and go to the support channel and we help you out!`);
@@ -13,8 +14,8 @@ module.exports = (client, guild) => {
                 } catch (error) {return}
                 finally {guild.leave()}
             }
-            else if (res?.bannedOwners.find(item => item.owner === guild.ownerID)) {
-                let item = res.bannedOwners.find(item => item.owner === guild.ownerID);
+            else if (res.bannedOwners.find(item => item === guild.ownerID)) {
+                let item = res.bannedOwners.find(item => item === guild.ownerID);
                 try {
                     if (sysChan.permissionOverwrites.get(client.user.id).allow.has("SEND_MESSAGES")) {
                         sysChan.send(`Hey! The owner of this server is banned from using this bot.\n**Reason**: ${item.reason}\n\nThink this is a mistake? Join https://discord.gg/k2PEWMw and go to the support channel and we help you out!`);
