b97c03cd402c09d4c0346d7bb810ef6049cd8900
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-03 11:30:45 +0200

    Update unbanowner.js

--- commands/unbanowner.js
+++ commands/unbanowner.js
@@ -25,7 +25,7 @@ module.exports = {
                 return message.channel.send(embed.description).catch(err => err);
             }
         }
-        var user = usr[0];
+        var user = usr;
         if (!user) {
             let embed = new MessageEmbed()
             .setColor(branding)
