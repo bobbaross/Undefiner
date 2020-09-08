826df15f1128c588ceddbbf4b45a61a6c263f586
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-09-08 16:47:55 +0200

    Added listing for blocked roles and channels

---
+++ commands/blockedroles.js
@@ -0,0 +1,43 @@
+const {MessageEmbed} = require('discord.js');
+const {branding} = require('../config.json').colors;
+
+module.exports = {
+    name: "blockedroles",
+    description: "Get the blocked roles for competitions.",
+    usage: "[page]",
+    aliases: [""],
+    category: "competition",
+    guildOnly: true,
+
+    async undefine(client, message, args, hasEmbedPerms) {
+        client.functions.getDB(message.guild.id).then(async res => {
+            if (!res) res = await client.functions.createDB(message.guild.id);
+            var page = args.shift();
+            if (page && !isNaN(page)) page = parseInt(page);
+            else page = 1;
+            if (res.comp.blockedRoles.length === 0) {
+                let embed = new MessageEmbed()
+                .setColor(branding)
+                .setDescription(`No roles blocked`)
+                if (hasEmbedPerms === true) {
+                    return message.channel.send(embed).catch(err => err);
+                } else {
+                    return message.channel.send(embed.description).catch(err => err);
+                }
+            }
+            var items = await client.functions.getPages(res.comp.blockedRoles, page);
+            var result = [];
+            for (let item of items.pages) {
+                result.push(`Name: ${message.guild.roles.cache.get(item.id).name}\nID: ${item.id}`);
+            }
+            let embed = new MessageEmbed()
+            .setColor(branding)
+            .setDescription(`Current blocked roles:\n${result.join('\n')}\nPage ${items.amount}`)
+            if (hasEmbedPerms === true) {
+                return message.channel.send(embed).catch(err => err);
+            } else {
+                return message.channel.send(embed.description).catch(err => err);
+            }
+        });
+    }
+}
